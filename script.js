28889a6b3ad11d11619524fb08641f092f898202
0x666690 <87996468+0x666690@users.noreply.github.com>
2023-02-02 14:45:17 +0100

    emoji selectors

--- script.js
+++ script.js
@@ -24,25 +24,27 @@ function limitKeypress(event, value, maxLength) {
 }
 
 function submitSecretPassword() {
-    if (pw = document.getElementById("secret_password").value, "" != pw && null != pw) {
-        var t = new XMLHttpRequest;
-        t.open("POST", "https://treasure-hunt.z8.re/secret?password=" + pw), t.onload = function() {
-            if (200 == t.status) {
-                document.getElementsByClassName("button")[0].innerHTML = "Success!";
-                location.href = t.responseText;
-                document.getElementById("secret_dialog").style.display = "none";
-                document.getElementById("secret_dialog").innerHTML = "";
-            }
-            else if (401 == t.status) {
-                //document.getElementsByClassName("button")[0].innerHTML = "You think you're clever, huh?";
-                document.getElementById("secret_dialog").innerHTML = t.responseText;
-                document.getElementById("secret_dialog").style.display = "block";
-            }
-            else if (400 == t.status) {
-                document.getElementsByClassName("button")[0].innerHTML = "Wrong password!";
-                document.getElementById("secret_dialog").style.display = "none";
-                document.getElementById("secret_dialog").innerHTML = "";
-            }
-        }, t.send()
-    }
+    const emoji1 = document.querySelector("#emoji1").value;
+    const emoji2 = document.querySelector("#emoji2").value;
+    const emoji3 = document.querySelector("#emoji3").value;
+    const emoji4 = document.querySelector("#emoji4").value;
+    const str = emoji1 + emoji2 + emoji3 + emoji4;
+    var t = new XMLHttpRequest;
+    t.open("POST", "https://treasure-hunt.z8.re/secret?password=" + str), t.onload = function() {
+        if (200 == t.status) {
+            document.getElementsByClassName("button")[0].innerHTML = "Success!";
+            location.href = t.responseText;
+            document.getElementById("secret_dialog").style.display = "none";
+            document.getElementById("secret_dialog").innerHTML = "";
+        }
+        else if (401 == t.status) {
+            document.getElementById("secret_dialog").innerHTML = t.responseText;
+            document.getElementById("secret_dialog").style.display = "block";
+        }
+        else if (400 == t.status) {
+            document.getElementsByClassName("button")[0].innerHTML = "Wrong password!";
+            document.getElementById("secret_dialog").style.display = "none";
+            document.getElementById("secret_dialog").innerHTML = "";
+        }
+    }, t.send()
     }

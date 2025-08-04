4f5dcdbe1bbc03ec7b0a11c3ad08f9d7ce4ecb4a
camoufly <82298951+camoufly@users.noreply.github.com>
2025-08-04 12:37:24 +0200

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -12,7 +12,7 @@ const EMAILS_FILE = "emails.txt";
 
 export default async function handler(req, res) {
   // CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "https://camoufly.me");
+  res.setHeader("Access-Control-Allow-Origin", *);
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 

632f5dc99caa123b388ff7c28519f5dcf17b35d7
camoufly <82298951+camoufly@users.noreply.github.com>
2025-08-07 23:45:04 +0200

    Create test-token.js

---
+++ api/test-token.js
@@ -0,0 +1,12 @@
+// pages/api/test-token.js
+import { getAccessToken } from "../../lib/dropbox";
+
+export default async function handler(req, res) {
+  try {
+    const token = await getAccessToken();
+    res.status(200).json({ token });
+  } catch (err) {
+    console.error("token error:", err);
+    res.status(500).json({ error: err.message });
+  }
+}
